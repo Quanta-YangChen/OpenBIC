6f9bc644c81d80f52b318d811341d4154001ac8b
Yang.Chen <Yang.Chen@quantatw.com>
2022-05-13 16:00:42 +0800

    GT: Version commit for GT-SWB-2022.19.01
    Summary:
    - Version commit for GT switch board BIC GT-SWB-2022.19.01

--- meta-facebook/gt-cc/src/platform/plat_version.h
+++ meta-facebook/gt-cc/src/platform/plat_version.h
@@ -15,7 +15,7 @@
 
 #define BIC_FW_YEAR_MSB 0x20
 #define BIC_FW_YEAR_LSB 0x22
-#define BIC_FW_WEEK 0x16
+#define BIC_FW_WEEK 0x19
 #define BIC_FW_VER 0x01
 #define BIC_FW_platform_0 0x47 // char: G
 #define BIC_FW_platform_1 0x54 // char: T
